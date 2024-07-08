f3e8369806a16b4bf21fa7ca89a30ad13fda6646
jared <jaredlight18@gmail.com>
2024-07-09 00:52:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<button @click="text = 123">click me {{ text }}</button>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const text = ref("Hello World!")
+</script>
